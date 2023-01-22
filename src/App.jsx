053b8006fc18744aed5b5b0afede17922af31665
carzborn <60917627+carzborn@users.noreply.github.com>
2023-01-22 09:53:49 +0100

    all routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navigation';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage'
-import Login from './pages/LoginPage';
+import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage'
+import NotFoundPage from './pages/NotFoundPage'
+import { useAuthContext } from './contexts/AuthContext';
+import UpdateProfile from './pages/UpdateProfilePage';
 
 function App() {
-
+  const {currentUser} = useAuthContext();
   return (
     <div id='App'>
+      {/* <Navbar/> */}
       <Routes>
-        <Route path='/' element={<RegisterPage/>}/>
-        <Route path='/Login' element={<Login/>}/>
+      <Route path="*" element={<NotFoundPage/>} />
+        <Route path='/register' element={<RegisterPage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        
+        <Route path='/' element={!currentUser? <Navigate to="/login" /> : <HomePage />}/>
+        <Route path='/update-profile' element={!currentUser? <Navigate to="/login" /> : <UpdateProfile/>}/>
+        
       </Routes>
     </div>
   )
