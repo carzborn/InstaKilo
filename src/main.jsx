4a15eb2176a1a2b4e63b5d59eed95bcae5ec7a78
carzborn <60917627+carzborn@users.noreply.github.com>
2023-01-22 09:59:33 +0100

    misc

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MantineProvider withGlobalStyles withNormalizeCSS theme={{colorScheme: 'dark',         shadows: {
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={{colorScheme: 'dark', shadows: {
           md: '1px 1px 3px rgba(0, 0, 0, .25)',
           xl: '5px 5px 3px rgba(0, 0, 0, .25)',
         },}}>
